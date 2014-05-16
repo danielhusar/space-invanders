655df152e18d7a4f84b1747524690d5a17012ca7
Daniel Husar <dano.husar@gmail.com>
2014-05-16 10:46:16 +0100

    movement

--- app.js
+++ app.js
@@ -6,19 +6,35 @@
     this.context = this.canvas.getContext('2d');
     this.width = $('canvas').width();
     this.height = $('canvas').height();
+    this.keys = [];
 
     this.plane = {
       x: this.width/2 - 25,
-      y: this.height - 80
+      y: this.height - 80,
+      step: 3
     };
 
     this.init();
+    this.bindings();
   };
 
   var FN = APP.prototype;
 
   FN.init = function(){
     this.drawPlane(this.plane.x,  this.plane.y);
+
+    if (this.keys[37]) {
+      this.plane.x -= this.plane.step;
+      if(this.plane.x < 0){
+        this.plane.x = 0;
+      }
+    }
+    if (this.keys[39]) {
+      this.plane.x += this.plane.step;
+      if(this.plane.x > this.width - 50){
+        this.plane.x = this.width - 50;
+      }
+    }
   };
 
   FN.drawPlane = function(x, y){
@@ -28,6 +44,15 @@
       this.context.drawImage(img, x, y);
     }.bind(this);
     img.src = 'assets/space-ship.png';
+  };
+
+  FN.bindings = function(){
+    $(document).keydown(function(e){
+      this.keys[e.keyCode] = true;
+    }.bind(this));
+    $(document).keyup(function(e){
+      this.keys[e.keyCode] = false;
+    }.bind(this));
   };
 
 
